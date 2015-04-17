f92eacdda666724e377c2f092f874a1715960474
CHADA <chada.said@ymail.com>
2015-04-18 01:08:27 +0200

    added local jquery and custom app.js

---
+++ store/web/assets/backend/javascripts/app.js
@@ -0,0 +1,37 @@
+function updateActive($link, response){
+    if($link instanceof $){
+        var original_url = $link.attr('href');
+        var switch_url = $link.attr('data-url');
+        $link.attr('href',switch_url);
+        $link.attr('data-url',original_url);
+        $link.children('i').toggleClass('fa fa-check');
+        $link.children('i').toggleClass('fa fa-times');
+        //Parcourt les message de la response et les affiche
+        $.each(response.messages[response.template],function(key,msg){
+            var $flash = $('\
+                        <div class="alert alert-dark alert-'+ response.template + '">\
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+                        <span aria-hidden="true">&times;</span>\
+                        </button>' + msg + '</div>")'
+            );
+            var $content = $('#content-wrapper');
+            $content.find('.alert.alert-dark').remove();
+            $content.prepend($flash).hide.fadeIn();
+        });
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//Adding event listener to activate-element
+$('.activable-element a').on('click', function(e) {
+    e.preventDefault();
+    var $link = $(this);
+    $.ajax({
+        url: $link.attr('href')
+    }).done(function (response) {
+        updateActive($link,response);
+    })
+});
