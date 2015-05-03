9e074618fb810afebddc03c1a12407fa59901ae1
CHADA <chada.said@ymail.com>
2015-05-03 13:51:20 +0200

    added StoreFrontendBundle

---
+++ store/src/Store/FrontendBundle/StoreFrontendBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asus
+ * Date: 03/05/2015
+ * Time: 13:48
+ */
+
+namespace Store\FrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StoreFrontendBundle extends Bundle {
+
+}
